9eba779d84fda7606cc80045ba9e828a85525dfd
Nikhil Mane <mane.nikhil92@gmail.com>
2023-04-10 13:34:01 +0530

    Daily

--- src/com/nikhil/collection/ComparableDemo.java
+++ src/com/nikhil/collection/ComparableDemo.java
@@ -27,15 +27,11 @@ class Student implements Comparable<Student>
 }
 public class ComparableDemo
 {
-
-
     public static void main(String[] args) {
         Student s1 = new Student(1,"Nikhil", 27);
         Student s2 = new Student(23,"Aish", 26);
         Student s3 = new Student(12,"Nisur", 34);
         Student s4 = new Student(45, "Rawam", 34);
-
-
         Student[] arr = {s1,s2,s3,s4};
         Arrays.sort(arr);
         for (Student student : arr) {
